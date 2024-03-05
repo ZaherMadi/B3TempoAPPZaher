c4724ca7dd952a5d5f871b6680e8112f2f644fab
Zaher Madi <zahermadi@yahoo.fr>
2024-03-05 09:40:02 +0100

    Get API 200

--- app/src/main/java/com/example/b3tempoappzaher/IEdfApi.java
+++ app/src/main/java/com/example/b3tempoappzaher/IEdfApi.java
@@ -1,31 +1,25 @@
 package com.example.b3tempoappzaher;
-import com.journaldev.retrofitintro.pojo.MultipleResource;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import com.journaldev.retrofitintro.pojo.MultipleResource;
-import com.journaldev.retrofitintro.pojo.User;
-import com.journaldev.retrofitintro.pojo.UserList;
+
+import com.example.b3tempoappzaher.TempoDaysLeft;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Query;
+
+
 public interface IEdfApi {
-    @GET("/services/rest/referentiel/getNbTempoDays")
-    Call<TempoDaysLeft> getTempoDaysLeft(@Query("TypeAlerte") String TypeAlerte);
+    String EDF_TEMPO_API_ALERT_TYPE = "TEMPO";
 
-    @GET("/services/rest/referentiel/getNbTempoDays")
-    Call<TempoDaysLeft> getTempoDaysLeft(@Query("TypeAlerte") String TypeAlerte);
+    // https://particulier.edf.fr/services/rest/referentiel/getNbTempoDays?TypeAlerte=TEMPO
+
+    @GET("services/rest/referentiel/getNbTempoDays")
+    Call<TempoDaysLeft> getTempoDaysLeft(@Query("TypeAlerte")String alertType);
+
+    // https://particulier.edf.fr/services/rest/referentiel/searchTempoStore?dateRelevant=2024-02-20&TypeAlerte=TEMPO
+    @GET("services/rest/referentiel/searchTempoStore")
+    Call<TempoDaysColor> getTempoDaysColor(
+            @Query("dateRelevant") String dateBegin,
+            @Query("TypeAlerte")String alertType);
+
+    // https://particulier.edf.fr/services/rest/referentiel/historicTEMPOStore?dateBegin=2023&dateEnd=2024
 }
-
-// https://particulier.edf.fr/services/rest/referentiel/getNbTempoDays?TypeAlerte=TEMPO
-// https://particulier.edf.fr/services/rest/referentiel/searchTempoStore?dateRelevant=2024-02-20&TypeAlerte=TEMPO
-//https://particulier.edf.fr/services/rest/referentiel/historicTEMPOStore?dateBegin=2023&dateEnd=2024
-
-
-
--- app/src/main/java/com/example/b3tempoappzaher/MainActivity.java
+++ app/src/main/java/com/example/b3tempoappzaher/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.b3tempoappzaher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.http.HttpResponseCache;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.net.HttpURLConnection;
+import java.time.LocalDate;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    IEdfApi edfApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofitClient = ApiClient.get();
+        if (retrofitClient != null) {
+            edfApi = retrofitClient.create(IEdfApi.class);
+        } else {
+            Log.e(LOG_TAG, "unable to initialize Retrofit client");
+            finish();
+        }
+
+        Call<TempoDaysLeft> call = edfApi.getTempoDaysLeft(IEdfApi.EDF_TEMPO_API_ALERT_TYPE);
+
+        call.enqueue(new Callback<TempoDaysLeft>() {
+            @Override
+            public void onResponse(@NonNull Call<TempoDaysLeft> call, @NonNull Response<TempoDaysLeft> response) {
+                TempoDaysLeft tempoDaysLeft = response.body();
+                if (response.code() == HttpURLConnection.HTTP_OK && tempoDaysLeft != null) {
+                    Log.d(LOG_TAG, "nb red days = " + tempoDaysLeft.getParamNbJRouge());
+                    Log.d(LOG_TAG, "nb white days = " + tempoDaysLeft.getParamNbJBlanc());
+                    Log.d(LOG_TAG, "nb blue days = " + tempoDaysLeft.getParamNbJBleu());
+                } else {
+                    Log.w(LOG_TAG, "call to getTempoDaysLeft() failed with error code " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<TempoDaysLeft> call, @NonNull Throwable t) {
+                Log.e(LOG_TAG, "call to getTempoDaysLeft () failed ");
+            }
+        });
+
+        Call<TempoDaysColor> call2 = edfApi.getTempoDaysColor("2024-03-05", IEdfApi.EDF_TEMPO_API_ALERT_TYPE);
+
+        call2.enqueue(new Callback<TempoDaysColor>() {
+
+            @Override
+            public void onResponse(@NonNull Call<TempoDaysColor> call, @NonNull Response<TempoDaysColor> response) {
+                TempoDaysColor tempoDaysColor = response.body();
+                if (response.code() == HttpURLConnection.HTTP_OK && tempoDaysColor != null) {
+                    Log.d(LOG_TAG, "Today color = " + tempoDaysColor.getCouleurJourJ());
+                    Log.d(LOG_TAG, "Tomorrow color = " + tempoDaysColor.getCouleurJourJ1());
+                } else {
+                    Log.w(LOG_TAG, "call to getTempoDaysColor() failed with error code " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<TempoDaysColor> call, @NonNull Throwable t) {
+                Log.e(LOG_TAG, "call to getTempoDaysColor() failed ");
+            }
+        });
     }
 }
